4a146fb0d592b70fc6cac77cbf4c3bda1d2faabe
Nigel Bray <httpsroot@email.to1.uk>
2024-01-13 10:00:43 +0000

    nixos convention

--- nixos/dubedary/configuration.nix
+++ nixos/dubedary/configuration.nix
@@ -8,7 +8,7 @@
   };
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
   boot.initrd.secrets = { "/crypto_keyfile.bin" = null; };
   boot.initrd.luks.devices."partition_uuid".device = "/dev/disk/by-uuid/partition_uuid";
   boot.initrd.luks.devices."partition_uuid".keyFile = "/crypto_keyfile.bin";
