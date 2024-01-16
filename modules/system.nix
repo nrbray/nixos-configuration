355c2fb166b0214c5dbd2c740fc6565e3fc4f460
Nigel Bray <httpsroot@email.to1.uk>
2024-01-16 21:31:06 +0000

    avingate syncthing module

--- modules/system.nix
+++ modules/system.nix
@@ -1,7 +1,5 @@
-{ pkgs, lib, ... }: { 
-
+{ pkgs, lib, ... }: { # flakes clean-tmp-on-boot locale unfree select-pkgs root-sshd-certificate-only
   nix.settings.experimental-features = ["nix-command" "flakes"];
-
   boot.tmp.cleanOnBoot = true;
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
@@ -13,5 +11,5 @@
   environment.systemPackages = with pkgs; [ rsync git tmux ripgrep ];
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "prohibit-password";
-  services.sshd.enable = true;
+  services.sshd.enable = true; # sshd is the OpenSSH server process
 }
