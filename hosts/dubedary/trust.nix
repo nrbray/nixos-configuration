d9f140541836a83df7c3183d65fa47a54f0dd1d4
Nigel Bray <httpsroot@email.to1.uk>
2024-01-21 11:14:48 +0000

    so many gotchas

--- hosts/dubedary/trust.nix
+++ hosts/dubedary/trust.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }: { 
   users.users.nrb = {
+    extraGroups = [  "networkmanager" "wheel" ];
     packages = with pkgs; [];
     # trust on this machine  
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGXa5qbZS3vXSkT4EcJDMp2IBOmeI0pu20wtHEiGb5A" ];  
