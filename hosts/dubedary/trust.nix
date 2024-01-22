6bc6bf809cf150759d207e4cd79f3a5180dd3b24
Nigel Bray <httpsroot@email.to1.uk>
2024-01-22 20:45:52 +0000

    fix string, keep alignment with minatanin

--- hosts/dubedary/trust.nix
+++ hosts/dubedary/trust.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }: { 
   users.users.nrb = {
     extraGroups = [  "networkmanager" "wheel" ];
-    openssh.authorizedKeys.keys = [ "
-      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGXa5qbZS3vXSkT4EcJDMp2IBOmeI0pu20wtHEiGb5A" 
+    openssh.authorizedKeys.keys = [ 
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGXa5qbZS3vXSkT4EcJDMp2IBOmeI0pu20wtHEiGb5A"
       
       ];  
   };
