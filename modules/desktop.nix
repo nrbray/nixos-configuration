299d6a815abaa50e67e1982c51dcdf9599207bdd
Nigel Bray <nixconfig.dubedary@email.to1.uk>
2024-10-25 09:34:43 +0100

    nixos rebuild changes

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -2,7 +2,7 @@
   services.xserver.enable = true;  # Enable the X11 windowing system.
   services.xserver.displayManager.gdm.enable = true;  # login display manager https://unix.stackexchange.com/questions/131496/what-is-lightdm-and-gdm
   services.xserver.desktopManager.gnome.enable = true; # Enable the GNOME Desktop Environment. 
-  services.xserver = { layout = "gb"; xkbVariant = ""; };  # Configure keymap in X11
+  services.xserver = { xkb.layout = "gb"; xkb.variant = ""; };  # Configure keymap in X11
   sound.enable = true;  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
