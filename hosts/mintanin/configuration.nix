60f2ecb3a031c8ac18b8eeaca81ee8519df1c593
Nigel Bray <httpsroot@email.to1.uk>
2024-11-24 18:55:42 +0000

    update mintantin

--- hosts/mintanin/configuration.nix
+++ hosts/mintanin/configuration.nix
@@ -1,5 +1,11 @@
 { config, pkgs, lib, myappattribute, ... }:
 {
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.initrd.secrets = { "/crypto_keyfile.bin" = null; };
+  boot.initrd.luks.devices."luks-20a1f166-1f1d-4d97-99c2-a9260c817e32".device = "/dev/disk/by-uuid/20a1f166-1f1d-4d97-99c2-a9260c817e32";  # Enable swap on luks
+  boot.initrd.luks.devices."luks-20a1f166-1f1d-4d97-99c2-a9260c817e32".keyFile = "/crypto_keyfile.bin";
+  networking.hostName = "mintanin"; # Define your hostname.
+  system.stateVersion = "22.11"; # Did you read the comment?
   imports = [
     ./hardware-configuration.nix
     ../../modules/desktop.nix 
@@ -12,10 +18,4 @@
     ../../users/nrb.nix
     ../../users/git.nix
   ]; 
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.initrd.secrets = { "/crypto_keyfile.bin" = null; };
-  boot.initrd.luks.devices."luks-20a1f166-1f1d-4d97-99c2-a9260c817e32".device = "/dev/disk/by-uuid/20a1f166-1f1d-4d97-99c2-a9260c817e32";  # Enable swap on luks
-  boot.initrd.luks.devices."luks-20a1f166-1f1d-4d97-99c2-a9260c817e32".keyFile = "/crypto_keyfile.bin";
-  networking.hostName = "mintanin"; # Define your hostname.
-  system.stateVersion = "22.11"; # Did you read the comment?
 }
